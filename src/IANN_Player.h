ef0b3325b9fc0c661332bb28b71fcc35cd68ef49
Kaikou Loic Degni <>
2026-04-19 16:18:36 -0400

    Refactoring de la structure de fichier

--- src/IANN_Player.h
+++ src/IANN_Player.h
@@ -21,7 +21,7 @@ class IANN_Player : public Player_Interface {
     char _player;
     unsigned int _taille;
     unsigned int _time_limit_ms = 2000; // Par défaut, 2 secondes par coup
-    std::vector<char>>  _board;
+    std::vector<char> _board;
     
     std::vector< std::tuple<unsigned int, unsigned int, char> > _historique_coups;
     //std::vector<TrainingExample>* _training_examples;
